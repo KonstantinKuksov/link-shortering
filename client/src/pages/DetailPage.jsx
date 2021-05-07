f9e577a1651d2619fe645ce920b04876e325e18f
KonstantinKuksov <kkuksov@gmail.com>
2021-05-07 12:26:10 +0300

    work commit

--- client/src/pages/DetailPage.jsx
+++ client/src/pages/DetailPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useContext, useEffect } from 'react';
-import { useParams } from 'react-router';
+import { useParams } from 'react-router-dom';
 import { useHttp } from './../hooks/http.hook';
 import { AuthContext } from './../context/AuthContext';
 import { Loader } from './../components/Loader.jsx';
@@ -7,7 +7,7 @@ import { LinkCard } from './../components/LinkCard.jsx';
 
 export const DetailPage = () => {
   const { token } = useContext(AuthContext);
-  const { request, loading } = useHttp;
+  const { request, loading } = useHttp();
   const [link, setLink] = useState(null);
   const linkId = useParams().id;
 
